1b34b58acc92ee83dbca2ea99be22bf662ba385d
Justin Gomez <programjustin@gmail.com>
2018-04-09 10:56:36 -0700

    Add the Bootstrap CSS framework

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './App';
+import AddItem from './components/AddItem';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path='/' component={App} />
+      <Route path='/add-item' component={AddItem} />
     </div>
   </Router>,
   document.getElementById('root')
